0c552b0718f62b82a3abd74610b401a2fac6c2c6
cjayasanka <cjayasanka93@gmail.com>
2019-09-15 17:58:28 +0530

    Updates

---
+++ precache-manifest.e5f24bf48062a920c93103f17b6624d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b96dea85ee0fa46d40c3474082c3a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a639aa517bef7a6b17c1",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a3b60a1b44fc714b593c",
+    "url": "/static/js/2.8f03f605.chunk.js"
+  },
+  {
+    "revision": "a639aa517bef7a6b17c1",
+    "url": "/static/js/main.41d3cb99.chunk.js"
+  },
+  {
+    "revision": "b169a54161cf99c144ef",
+    "url": "/static/js/runtime~main.40f38a65.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
